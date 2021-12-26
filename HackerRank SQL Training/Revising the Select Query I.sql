97e1d42b3650237187f08164ec7df365bada62fe
ayushksingh11 <96696686+ayushksingh11@users.noreply.github.com>
2021-12-26 23:22:43 +0530

    Initial Commit

---
+++ HackerRank SQL Training/Revising the Select Query I.sql
@@ -0,0 +1,7 @@
+/* Retrieve all the columns from Table 'City' where Country Code is 'USA' & Population is greater than 100000*/
+SELECT 
+      * 
+FROM 
+    CITY
+WHERE 
+    COUNTRYCODE='USA' AND POPULATION>100000;
