431b72222c2213c8ada0f2c2b6090ce6ae7442a3
ayushksingh11 <96696686+ayushksingh11@users.noreply.github.com>
2021-12-27 00:44:06 +0530

    Initial Commit

---
+++ HackerRank SQL Training/Type of Triangle.sql
@@ -0,0 +1,16 @@
+select
+	case
+		when (A + B <= C
+		or A + C <= B
+		or B + C <= A) then 'Not A Triangle'
+		when (A = B
+		and B = C) then 'Equilateral'
+		when (A = B
+		or A = C
+		or B = C) then 'Isosceles'
+		when (A <> B
+		and B <> C
+		and C <> A) then 'Scalene'
+	end tuple
+from
+	TRIANGLES;
