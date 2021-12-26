bec0b2dd25bcdcfd45b91955b7137230b7b3a4d8
ayushksingh11 <96696686+ayushksingh11@users.noreply.github.com>
2021-12-27 00:33:37 +0530

    Initial Commit

---
+++ HackerRank SQL Training/Weather Observation Station 20.sql
@@ -0,0 +1,18 @@
+select
+	round(S.LAT_N, 4) mediam
+from
+	station S
+where
+	(
+	select
+		count(Lat_N)
+	from
+		station
+	where
+		Lat_N < S.LAT_N ) = (
+	select
+		count(Lat_N)
+	from
+		station
+	where
+		Lat_N > S.LAT_N)
